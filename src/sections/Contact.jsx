ce2047f675c209abc6b54aedb58a1d8febc1db7d
damayantida <damayanti2388@gmail.com>
2025-02-08 07:05:10 +0700

    Edit margin Contact section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
 	return (
 		<Element name='contact'>
 			<section className='bg-gray-800 py-16 px-10 border-t-5 border-white text-white'>
-				<div className='container px-6 text-center flex flex-col items-center'>
+				<div className='container px-6 text-center flex flex-col items-center justify-center mt-8'>
 					<h2 className='text-white text-3xl lg:text-4xl font-bold mb-8 lg:mb-10'>
 						Contact Me
 					</h2>
