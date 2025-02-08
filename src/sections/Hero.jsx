4fd3cf184d1c400db50e075ac707720baf13ab55
damayantida <damayanti2388@gmail.com>
2025-02-08 08:11:21 +0700

    Edit Hero content positioning

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
 			<section className='relative pt-30 pb-20 max-lg:pt-26 max-lg:pb-18 max-md:pt-18 max-md:pb-16 bg-blue-400'>
 				<div className='container flex justify-center'>
 					<div className='w-120 max-sm:w-108 max-sm:h-96 h-107 bg-booting bg-contain bg-no-repeat relative'>
-						<div className='flex flex-col justify-center items-center gap-2 absolute top-10 max-sm:top-1 left-1 max-sm:left-0 h-93 max-sm:h-86 w-108 max-sm:w-100'>
+						<div className='flex flex-col justify-center items-center gap-2 absolute top-10 max-sm:top-0 -left-3 max-sm:left-0 h-93 max-sm:h-86 w-108 max-sm:w-100'>
 							<img
 								src='/hero-computer.svg'
 								alt='computer'
