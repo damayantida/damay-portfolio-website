e42f697a2c39e2f1a0a32b9d6d18f7bb190f1ddd
damayantida <damayanti2388@gmail.com>
2025-02-10 13:21:55 +0700

    Chane padding contact section

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
 	return (
 		<section className='bg-gradient-to-t from-black to-gray-900 py-16 border-t border-white text-white'>
 			<Element name='contact'>
-				<div className='container px-6 text-center flex flex-col items-center justify-center mt-8 w-screen'>
+				<div className='container text-center flex flex-col items-center justify-center mt-8 w-screen'>
 					<h2 className='text-white text-3xl lg:text-4xl font-bold mb-8 lg:mb-10'>
 						Contact Me
 					</h2>
